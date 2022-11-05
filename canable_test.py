5497d5506e535d537ddd6a5ea3a5df6517f115f9
Pako2425 <patryk.kocinski340@gmail.com>
2022-11-05 11:39:51 +0100

    rename fh4 python file

--- canable_test.py
+++ canable_test.py
@@ -3,13 +3,13 @@ import time
 
 bus = can.interface.Bus(bustype='slcan', channel='COM3', bitrate=125000)
 
-msg = can.Message(arbitration_id=0x128, data=[128,0,0,0,0,0,0,0], is_extended_id=False, dlc=8)
-fuel = can.Message(arbitration_id=0x0F6, data=[0,0,0,0,0,0,0,0], is_extended_id=False, dlc=8)
+msg = can.Message(arbitration_id=0x128, data=[0,90,0,0,0,0,0,0], is_extended_id=False, dlc=8)
+fuel = can.Message(arbitration_id=0x0B6, data=[50,255,50,255,0,0,0,0], is_extended_id=False, dlc=8)
 
 counter = 0
 while(True):
     try:
-        bus.send(fuel)
+        bus.send(msg)
         print("Message sent")
     except can.CanError:
         print("Message NOT sent")
