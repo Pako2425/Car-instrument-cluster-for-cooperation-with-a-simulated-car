d64b9b2d6dfd1750317d0323af64ba6644d52009
Pako2425 <patryk.kocinski340@gmail.com>
2022-12-03 01:39:54 +0100

    najlepiej działający prototyp

--- canable_test.py
+++ canable_test.py
@@ -26,65 +26,25 @@ handbreak = 0
     #    print("Message NOT sent")
     #
     #time.sleep(1.0)
-
+warning_lights = [8, 0, 0, 0, 0, 0, 0, 0]
+i = 7
 while True:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
-                if speed < 65000:
-                    speed = speed + 1000
+                warning_lights[i] = (warning_lights[i] + 1)%256
             elif event.key == pygame.K_s:
-                if speed > 0:
-                    speed = speed - 1000
-            elif event.key == pygame.K_l:
-                light = (light+1) % 2
-            elif event.key == pygame.K_a:
-                if gear < 5:
-                    gear = gear + 1
-            elif event.key == pygame.K_z:
-                if gear > 0:
-                    gear = gear - 1
-            elif event.key == pygame.K_LEFT:
-                left = (left + 1) % 2
-            elif event.key == pygame.K_RIGHT:
-                right = (right + 1) % 2
-            elif event.key == pygame.K_SPACE:
-                handbreak = (handbreak + 1) % 2
+                warning_lights[i] = (warning_lights[i] - 1) % 256
+    print(bin(warning_lights[i]))
 
-    a = speed & 0xFF
-    b = (speed >> 8) & 0xFF
-    light_and_gear_data = [0,0,0,0,0,0,0,0]
-    light_and_gear_data[0] = light*64 + left*2 + right*4
-    if gear == 0:
-        light_and_gear_data[1] = 16
-    elif gear == 1:
-        light_and_gear_data[1] = 144
 
-    elif gear == 2:
-        light_and_gear_data[1] = 128
-
-    elif gear == 3:
-        light_and_gear_data[1] = 112
-
-    elif gear == 4:
-        light_and_gear_data[1] = 96
-
-    elif gear == 5:
-        light_and_gear_data[1] = 80
-
-    else:
-        light_and_gear_data[1] = 16
-
-    print(light)
-    speed_and_rpm = can.Message(arbitration_id=0x0B6, data=[b, a, b, a, 0, 0, 0, 0], is_extended_id=False, dlc=8)
-    msg_light_and_gear = can.Message(arbitration_id=0x128, data=light_and_gear_data, is_extended_id=False, dlc=8)
-    msg_warning_lights = can.Message(arbitration_id=0x168, data=[0, 0, 0, 1, 0, 0, 0, 0], is_extended_id=False, dlc=8)
+    msg_temperatureGauge = can.Message(arbitration_id=0x0F6, data=warning_lights, is_extended_id=False, dlc=8)
 
     try:
-        bus.send(speed_and_rpm)
-        bus.send(msg_light_and_gear)
-        bus.send(msg_warning_lights)
+        #bus.send(speed_and_rpm)
+        bus.send(msg_temperatureGauge)
+        #bus.send(msg_warning_lights)
         print("Message sent")
     except can.CanError:
         print("Message not sent, ERROR")
-    time.sleep(0.1)
+    time.sleep(0.05)
--- fh4_receiver.py
+++ fh4_receiver.py
@@ -2,10 +2,104 @@ import socket
 import can
 import time
 from timeit import default_timer as timer
-import threading
 import struct
 import pygame
+from abc import ABC, abstractmethod
 
+
+class AbstractHandler(ABC):
+    def __init__(self, next, socket, bus):
+        self.next = next
+        self.sock = socket
+        self.bus = bus
+
+    @abstractmethod
+    def processData(self, data):
+        pass
+
+    #@abstractmethod
+    def handle(self, data=0):
+        data = self.processData(data)
+        if self.next is None:
+            return
+        else:
+            self.next.handle(data)
+
+class ReceiveData(AbstractHandler):
+    def processData(self, data):
+        recvData = self.sock.recvfrom(324)
+        return recvData[0]
+
+class DataProcessing(AbstractHandler):
+    def processData(self, data):
+        data_to_send = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+        data_to_send[0] = struct.unpack('f', data[16:20])[0]                #engineRPM
+        data_to_send[1] = struct.unpack('f', data[256:260])[0]              #speed
+        data_to_send[2] = struct.unpack('f', data[288:292])[0]              #fuel
+        data_to_send[3] = struct.unpack('f', data[292:296])[0]              #distanceTraveled
+        data_to_send[4] = struct.unpack('f', data[84:88])[0]                #flTireGrip
+        data_to_send[5] = struct.unpack('f', data[88:92])[0]                #frTireGrip
+        data_to_send[6] = struct.unpack('f', data[92:96])[0]                #blTireGrip
+        data_to_send[7] = struct.unpack('f', data[96:100])[0]               #brTireGrip
+        data_to_send[8] = struct.unpack('f', data[20:24])[0]                #accelerationX
+        data_to_send[9] = struct.unpack('f', data[28:32])[0]                #accelerationZ
+        data_to_send[10] = struct.unpack('H', data[318:319] + b'\x00')[0]   #handbrake
+        data_to_send[11] = struct.unpack('H', data[319:320] + b'\x00')[0]   #gear
+        return data_to_send
+
+class SendData(AbstractHandler):
+    def prepare_to_send(self, data):
+        rpm = int(data[0] * 8.200)
+        speed = int(data[1] * 357.142)
+        fuel_level = int(data[2] * 100.0)
+        print(fuel_level)
+        handBreak = data[10]
+
+        data_speedAndRpm = [(rpm >> 8 & 0xFF), (rpm & 0xFF), (speed >> 8 & 0xFF), (speed & 0xFF), 0, 0, 0, 0]
+
+        data_lightsAndGear = [0,0,0,0,0,0,0,0]
+        if data[11] == 0:
+            data_lightsAndGear[1] = 16
+        elif data[11] == 1:
+            data_lightsAndGear[1] = 144
+        elif data[11] == 2:
+            data_lightsAndGear[1] = 128
+        elif data[11] == 3:
+            data_lightsAndGear[1] = 112
+        elif data[11] == 4:
+            data_lightsAndGear[1] = 96
+        elif data[11] == 5:
+            data_lightsAndGear[1] = 80
+        else:
+            data_lightsAndGear[1] = 64
+
+        data_warningLights = [255, 255, 255, 16, 255, 255, 255, 255]
+
+        data_fuelGauge = [0, 0, 0, fuel_level, 0, 0, 0, 0]
+        data_temperature = [8,0,0,0,0,0,0,0]
+
+        return [data_speedAndRpm, data_lightsAndGear, data_fuelGauge, data_warningLights]
+
+    def send(self, data):
+        msg_fuelGauge = can.Message(arbitration_id=0x161, data=[2], is_extended_id=False, dlc=8)
+        msg_temperatureGauge = can.Message(arbitration_id=0x0F6, data=[8,0,0,0,0,0,0,0], is_extended_id=False, dlc=8)
+        msg_speedAndRpm = can.Message(arbitration_id=0x0B6, data=data[0], is_extended_id=False, dlc=8)
+        msg_lightsAndGear = can.Message(arbitration_id=0x128, data=data[1], is_extended_id=False, dlc=8)
+        msg_warning_lights = can.Message(arbitration_id=0x168, data=[0,0,0,1,0,0,0,0], is_extended_id=False, dlc=8)
+        try:
+            self.bus.send(msg_temperatureGauge)
+            self.bus.send(msg_fuelGauge)
+            self.bus.send(msg_speedAndRpm)
+            self.bus.send(msg_lightsAndGear)
+            self.bus.send(msg_warning_lights)
+
+            print("Message sent")
+        except can.CanError:
+            print("Message NOT sent")
+
+    def processData(self, data):
+        data_to_bus = self.prepare_to_send(data)
+        self.send(data_to_bus)
 
 class Receiver:
     def __init__(self, RECEIVER_IP, UDP_PORT):
@@ -14,153 +108,15 @@ class Receiver:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind((self.RECEIVER_IP, self.UDP_PORT))
         self.bus = can.interface.Bus(bustype='slcan', channel='COM3', bitrate=125000)
+        self.sd = SendData(None, None, self.bus)
+        self.dp = DataProcessing(self.sd, None, None)
+        self.r = ReceiveData(self.dp, self.sock, None)
 
-        self.data_speedAndRpm = [0,0,0,0,0,0,0,0]
-        self.data_lightsAndGear = [0,0,0,0,0,0,0,0]
-        self.data_warningLights = [0,0,0,0,0,0,0,0]
-
+        while True:
+            self.r.handle()
 
     def receive(self):
         recvData = self.sock.recvfrom(324)
-        self.telemetry = recvData[0]
+        return recvData[0]
 
-    def processingTelemetryData(self):
-        self.engineRpm = struct.unpack('f', self.telemetry[16:20])[0]
-        self.speed = struct.unpack('f', self.telemetry[256:260])[0]
-        self.fuel = struct.unpack('f', self.telemetry[288:292])[0]
-        self.distanceTraveled = struct.unpack('f', self.telemetry[292:296])[0]
-        self.flTireGrip = struct.unpack('f', self.telemetry[84:88])[0]
-        self.frTireGrip = struct.unpack('f', self.telemetry[88:92])[0]
-        self.blTireGrip = struct.unpack('f', self.telemetry[92:96])[0]
-        self.brTireGrip = struct.unpack('f', self.telemetry[96:100])[0]
-        self.accelerationX = struct.unpack('f', self.telemetry[20:24])[0]
-        self.accelerationZ = struct.unpack('f', self.telemetry[28:32])[0]
-        self.handBrake = struct.unpack('H', self.telemetry[318:319] + b'\x00')[0]
-        self.gear = struct.unpack('H', self.telemetry[319:320] + b'\x00')[0]
-
-    def processingDataForCluster(self):
-        #speed = int(self.speed * 1072.404)
-        speed = int(self.speed * 357.142)
-        rpm = int(self.engineRpm * 8.0)
-        self.data_speedAndRpm = [(rpm >> 8 & 0xFF), (rpm & 0xFF), (speed >> 8 & 0xFF), (speed & 0xFF), 0, 0, 0, 0]
-        if self.gear == 0:
-            self.data_lightsAndGear[1] = 16
-        elif self.gear == 1:
-            self.data_lightsAndGear[1] = 144
-
-        elif self.gear == 2:
-            self.data_lightsAndGear[1] = 128
-
-        elif self.gear == 3:
-            self.data_lightsAndGear[1] = 112
-
-        elif self.gear == 4:
-            self.data_lightsAndGear[1] = 96
-
-        elif self.gear == 5:
-            self.data_lightsAndGear[1] = 80
-
-        else:
-            self.data_lightsAndGear[1] = 16
-
-    def send(self):
-        msg_speedAndRpm = can.Message(arbitration_id=0x0B6, data=self.data_speedAndRpm, is_extended_id=False, dlc=8)
-        msg_lightsAndGear = can.Message(arbitration_id=0x128, data=self.data_lightsAndGear, is_extended_id=False, dlc=8)
-        msg_warning_lights = can.Message(arbitration_id=0x168, data=[0, 0, 0, 1, 0, 0, 0, 0], is_extended_id=False, dlc=8)
-        try:
-            self.bus.send(msg_speedAndRpm)
-            self.bus.send(msg_lightsAndGear)
-            self.bus.send(msg_warning_lights)
-            print("Message sent")
-        except can.CanError:
-            print("Message NOT sent")
-
-Receiver = Receiver("192.168.0.136", 5555)
-
-while True:
-    start = timer()
-    Receiver.receive()
-    Receiver.processingTelemetryData()
-    Receiver.processingDataForCluster()
-    Receiver.send()
-    end = timer()
-    print(end - start)
-
-
-
-
-#start = timer()
-#speed_and_rpm
-#speed = 10.0             #m/s
-#s = int(speed*1072.404)  #m/s to km/h and speed to cluster resolution
-#rpm = 4000.0
-#r = int(rpm*9.362)
-#data_speedAndRpm = [(s>>8 & 0xFF), (s & 0xFF), (r>>8 & 0xFF), (r & 0xFF), 0, 0, 0, 0]
-"""
-gear = 1
-light_and_gear_data = [0,0,0,0,0,0,0,0]
-if gear == 0:
-    light_and_gear_data[1] = 16
-elif gear == 1:
-    light_and_gear_data[1] = 144
-
-elif gear == 2:
-    light_and_gear_data[1] = 128
-
-elif gear == 3:
-    light_and_gear_data[1] = 112
-
-elif gear == 4:
-    light_and_gear_data[1] = 96
-
-elif gear == 5:
-    light_and_gear_data[1] = 80
-
-else:
-    light_and_gear_data[1] = 16
-
-end = timer()
-print(end - start)
-"""
-
-"""
-pygame.init()
-window_size = (400,400)
-window = pygame.display.set_mode(window_size)
-black = pygame.color.Color('#000000')
-green = pygame.color.Color('#32CD32')
-yellow = pygame.color.Color('#DDDDDD')
-font = pygame.font.Font(None, 30)
-
-while(True):
-    recvData = sock.recvfrom(324)
-    telemetry = recvData[0]
-
-    engineRpm = struct.unpack('f', telemetry[16:20])[0]
-    speed = struct.unpack('f', telemetry[256:260])[0]
-    fuel = struct.unpack('f', telemetry[288:292])[0]
-    distanceTraveled = struct.unpack('f', telemetry[292:296])[0]
-    flTireGrip = struct.unpack('f', telemetry[84:88])[0]
-    frTireGrip = struct.unpack('f', telemetry[88:92])[0]
-    blTireGrip = struct.unpack('f', telemetry[92:96])[0]
-    brTireGrip = struct.unpack('f', telemetry[96:100])[0]
-    accelerationX = struct.unpack('f', telemetry[20:24])[0]
-    accelerationZ = struct.unpack('f', telemetry[28:32])[0]
-    handBrake = struct.unpack('H', telemetry[318:319]+b'\x00')[0]
-    gear = struct.unpack('H', telemetry[319:320]+b'\x00')[0]
-
-    window.fill(black)
-    window.blit(font.render(f"engine_RPM: {format(engineRpm, '.3f')}", False, green), (20, 20))
-    window.blit(font.render(f"speed m/s: {format(speed, '.3f')}", False, green), (20, 45))
-    window.blit(font.render(f"fuel: {fuel}", False, green), (20, 70))
-    window.blit(font.render(f"distanceTraveled: {distanceTraveled}", False, green), (20, 95))
-    window.blit(font.render(f"flTireGrip: {format(flTireGrip, '.1f')}", False, green), (20, 120))
-    window.blit(font.render(f"frTireGrip: {format(frTireGrip, '.1f')}", False, green), (20, 145))
-    window.blit(font.render(f"blTireGrip: {format(blTireGrip, '.1f')}", False, green), (20, 170))
-    window.blit(font.render(f"brTireGrip: {format(brTireGrip, '.1f')}", False, green), (20, 195))
-    window.blit(font.render(f"acceX <-> m/s: {format(accelerationX, '.1f')}", False, green), (20, 220))
-    window.blit(font.render(f"acceY  ^  m/s: {format(accelerationZ, '.1f')}", False, green), (20, 245))
-    window.blit(font.render(f"handBrake: {handBrake}", False, green), (20, 270))
-    window.blit(font.render(f"gear: {gear}", False, green), (20, 295))
-    pygame.display.flip()
-"""
+Receiver = Receiver("192.168.8.102", 5555)
